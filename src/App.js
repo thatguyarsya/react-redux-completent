bfffdcb0ac0c3e77369ca7bbf7047f0411bcf47c
thatguyarsya <thatguyarsy@gmail.com>
2019-09-17 16:40:18 +0700

    homepage edit

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         create a to-do list <br /> <bold>****IN YOUR MIND*****</bold>
         </p>
         <a
           className="App-link"
