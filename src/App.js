b2ebc30914a3ee174bececf92bba00a96e8b00dd
thatguyarsya <thatguyarsy@gmail.com>
2019-09-17 16:23:31 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { combineReducers, createStore } from 'redux'
+
+
+const stateReducer = (state={}, action) => {
+  switch (action.type){
+    case 'change_state': {
+      state = {...state, state: action.dat}
+      break;
+    }
+    default: {
+      return state
+    }
+  }
+   return state
+};
+
+const androidReducer = (state=[], action) => {
+  switch (action.type){
+    case 'change_id': {
+      state = {...state, id: action.dat}
+      break;
+    }
+    default: {
+      return state
+    }
+  }
+    return state;
+};
+
+const reducers = combineReducers({
+   state: stateReducer,
+   android: androidReducer
+})
+
+const store = createStore(reducers);
+
+// 3. Subscribe if State Changes
+store.subscribe(() => {
+  console.log("incoming state alteration:", store.getState())
+})
+
+// 4. Dispatch an Action
+store.dispatch({type: 'change_id', dat:'008236'})
+store.dispatch({type: 'change_state', dat:'DEAD'})
+
+
 
 function App() {
   return (
